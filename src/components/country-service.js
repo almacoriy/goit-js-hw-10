1a4596517b4adcce8e2da8077df7a44efbd269da
almacoriy <amazon407@gmail.com>
2022-01-03 21:51:08 +0200

    fix country-service.js

--- src/components/country-service.js
+++ src/components/country-service.js
@@ -7,12 +7,8 @@ export default class CountryApiService {
     const url = `https://restcountries.com/v3.1/name/${this.searchForm}?fields=name,capital,population,flags,languages`;
 
     return fetch(url)
-      .then(response => {
-        return response;
-      })
-      .then(data => {
-        return data;
-      });
+      .then(response => response)
+      .then(data => data);
   }
   get form() {
     return this.searchForm;
